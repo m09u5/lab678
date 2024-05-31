210aba79be8d04922a28796e04b9b23138a7aadb
m09u5 <ryussexd@gmail.com>
2024-05-31 23:10:17 +0200

    added readjsonfile

---
+++ ReadJsonFile.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+def load_json(file_path):
+    print("doris")
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"Plik '{file_path}' nie istnieje.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"Niewłaściwy format pliku JSON: '{file_path}'.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    data = load_json(sys.argv[1])
+    print("Dane wczytane z pliku JSON:", data)
